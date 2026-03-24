dd91a63e9161d21ef99f4c597a9944f133af910c
plenilune4 <plenilune@hotmail.co.uk>
2026-03-24 12:22:42 +0000

    various files moved

--- frontend-service/front/src/App_original.tsx
+++ frontend-service/front/src/App_original.tsx
@@ -1,13 +1,14 @@
+import React from 'react';
 import logo from './logo.svg';
 import './App.css';
-
+import 'leaflet/dist/leaflet.css';
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Edit <code>src/App.tsx</code> and save to reload.
         </p>
         <a
           className="App-link"
@@ -15,11 +16,10 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Learn React, it's as easy as 4 5 6
         </a>
       </header>
-    </div>
-  );
+    </div>);
 }
 
 export default App;
